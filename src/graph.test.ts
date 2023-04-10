414bc383831ea4b1c491acdc216ab57b9b3ce4d9
Aaron Lipinski <kris.lipinski@gmail.com>
2023-04-10 11:21:32 +0200

    remove noisy logs

--- src/graph.test.ts
+++ src/graph.test.ts
@@ -164,21 +164,13 @@ describe('ngraph', () => {
         graph.addLink('c', 'b')
         graph.addLink('b', 'a')
 
-        graph.forEachLinkedNode(
-            'b',
-            function(node, link) { console.dir({node, link}) },
-            false
-        )
-
         const pathFinder = ngraphPath.aStar(graph, { oriented: true })
         const path = pathFinder.find('a', 'c').reverse()
 
-        console.dir(path)
         const makeLocker = makeMakeLocker<Node<Lock>>(node => node.data)
-        const lockNext = makeLocker("agent1", path, (nextNodes) => console.log("step", {nextNodes}))
+        const lockNext = makeLocker("agent1", path, (nextNodes) => {})
 
         for (var i = 0; i < path.length; i++) {
-            console.log("====== step =======")
             lockNext(path[i])
         }
     })
